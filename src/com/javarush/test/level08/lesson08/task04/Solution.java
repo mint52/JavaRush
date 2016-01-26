367adb411ef248881c71b9bf8d9d8b446fe41854
mint52 <kolyannow@gmail.com>
2016-01-26 12:26:36 +0400

    Тихонько Движется

--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.test.level08.lesson08.task04;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /* Удалить всех людей, родившихся летом
 Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
@@ -14,13 +16,32 @@ public class Solution
     {
         HashMap<String, Date> map = new HashMap<String, Date>();
         map.put("Stallone", new Date("JUNE 1 1980"));
+        map.put("Stallone1", new Date("JULY 1 1980"));
+        map.put("Stallone2", new Date("AUGUST 1 1980"));
+        map.put("Stallone3", new Date("SEPTEMBER 1 1980"));
+        map.put("Stallone4", new Date("OCTOBER 1 1980"));
+        map.put("Stallone5", new Date("APRIL 1 1980"));
+        map.put("Stallone6", new Date("MAY 1 1980"));
+        map.put("Stallone7", new Date("MARCH 1 1980"));
+        map.put("Stallone8", new Date("JANUARY 1 1980"));
+        map.put("Stallone9", new Date("JUNE 1 1980"));
 
         //напишите тут ваш код
-
+    return map;
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map)
     {
+        Iterator<Map.Entry<String, Date>> iterator =map.entrySet().iterator();
+
+        while (iterator.hasNext())
+        {
+            Map.Entry<String, Date> pair = iterator.next();
+            Date value = pair.getValue();
+            if (value.getMonth()>=5 && value.getMonth()<=7)
+            iterator.remove();
+
+        }
         //напишите тут ваш код
 
     }
