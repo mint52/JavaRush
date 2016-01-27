742ef9135d20210f85f0f012a0e608160261be66
mint52 <kolyannow@gmail.com>
2016-01-27 14:45:28 +0400

    ещё немного

---
+++ src/com/javarush/test/level05/lesson07/task05/Rectangle.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level05.lesson07.task05;
+
+/* Создать класс прямоугольник (Rectangle)
+Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height (левая координата, верхняя, ширина и высота). Создать для него как можно больше методов initialize(…)
+Примеры:
+-	заданы 4 параметра: left, top, width, height
+-	ширина/высота не задана (оба равны 0)
+-	высота не задана (равно ширине) создаём квадрат
+-	создаём копию другого прямоугольника (он и передаётся в параметрах)
+*/
+
+public class Rectangle
+{
+        int top;
+        int left;
+        int wigth;
+        int height;
+
+   public void initialize (int top, int left, int wigth, int height)
+   {
+       this.top = top;
+       this.left = left;
+       this.wigth = wigth;
+       this.height = height;
+   }
+   public void initialize (int top, int left)
+    {
+        this.top = top;
+        this.left = left;
+        this.wigth = 0;
+        this.height = 0;
+    }
+    public void initialize (int top, int left, int height)
+    {
+        this.top = top;
+        this.left = left;
+        this.wigth = height;
+        this.height = height;
+    }
+    public void initialize (Rectangle rect)
+    {
+        rect = this;
+    }
+
+    //напишите тут ваш код
+
+}
---
+++ src/com/javarush/test/level05/lesson09/task05/Rectangle.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level05.lesson09.task05;
+
+/* Создать класс прямоугольник (Rectangle)
+Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height (левая координата, верхняя, ширина и высота). Создать для него как можно больше конструкторов:
+Примеры:
+-	заданы 4 параметра: left, top, width, height
+-	ширина/высота не задана (оба равны 0)
+-	высота не задана (равно ширине) создаём квадрат
+-	создаём копию другого прямоугольника (он и передаётся в параметрах)
+*/
+
+public class Rectangle
+{
+    int top;
+    int left;
+    int wigth;
+    int height;
+
+    public Rectangle (int top, int left, int wigth, int height)
+    {
+        this.top = top;
+        this.left = left;
+        this.wigth = wigth;
+        this.height = height;
+    }
+    public Rectangle (int top, int left)
+    {
+        this.top = top;
+        this.left = left;
+        this.wigth = 0;
+        this.height = 0;
+    }
+    public Rectangle (int top, int left, int height)
+    {
+        this.top = top;
+        this.left = left;
+        this.wigth = height;
+        this.height = height;
+    }
+    public Rectangle (Rectangle rect)
+    {
+        rect = this;
+    }
+
+    //напишите тут ваш код
+
+
+}
