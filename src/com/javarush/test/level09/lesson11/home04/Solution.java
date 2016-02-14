5b5eeb130dbf2ba062a8609017d1ccb29d3257c6
mint52 <kolyannow@gmail.com>
2016-02-14 22:03:01 +0400

    q

--- src/com/javarush/test/level09/lesson11/home04/Solution.java
+++ src/com/javarush/test/level09/lesson11/home04/Solution.java
@@ -1,11 +1,5 @@
 package com.javarush.test.level09.lesson11.home04;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 /* Конвертер дат
 Ввести с клавиатуры дату в формате «08/18/2013»
 Вывести на экран эту дату в виде «AUG 18, 2013».
@@ -15,6 +9,9 @@ import java.util.Locale;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
+
+
+
         //напишите тут ваш код
     }
 }
