7328058788ca7302f7993264d4a2c5028f45a09b
mint52 <kolyannow@gmail.com>
2016-02-16 11:00:20 +0400

    1

---
+++ src/com/javarush/test/level13/lesson11/home04/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level13.lesson11.home04;
+
+/* Запись в файл
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой стороки.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String S = reader.readLine();
+        OutputStream os = new FileOutputStream(S);
+
+        while (true)
+        {
+            String s = reader.readLine();
+            if (s.equals("exit"));
+            {
+               break;
+            }
+            //int data = s.;
+            //os.write(data);
+        }
+
+
+    }
+}
